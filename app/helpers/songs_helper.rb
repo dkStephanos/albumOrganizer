0eba286791f625c5688337647add99d52a28caef
dkstephanos <stephanos@goldmail.etsu.edu>
2018-01-02 23:06:15 +0000

    Added helper method

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,4 +2,8 @@ module SongsHelper
     def user_song?(song)
         song.user == current_user ? true : false
     end
+    
+    def favorited(song)
+        song.is_favorite.to_s.capitalize
+    end
 end
