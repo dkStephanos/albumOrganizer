40012bbb924f928d43210ac70416ef890b62479c
dkstephanos <stephanos@goldmail.etsu.edu>
2018-01-09 13:54:18 +0000

    Refactored/Added partials

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -12,12 +12,4 @@ module ArtistsHelper
         !artist.genres.empty?
     end
     
-    # Only displays first link for some reason. Not used currently
-    def display_links(artist)
-        if artist.user == current_user
-            link_to "Add Album", new_artist_album_path(@artist) 
-            link_to "Edit Artist", edit_artist_path(@artist) 
-            link_to "Delete Artist", artist_path(@artist), method: :delete, data: { confirm: 'Are you certain you want to delete this?' }
-        end
-    end
 end
