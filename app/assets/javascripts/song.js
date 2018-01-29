8341cc3c97c185e7fe4f58884c2e9c7f2ced5837
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-01-29 14:12:16 -0500

    Added 'Last Song' link to song#show and corresponding JS function to song.js

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -9,37 +9,67 @@ function attachListeners() {
         event.preventDefault();
         nextSong();
     });
-    //Attaches listener for 'Previous Song' link
-
+    //Attaches listener for 'Last Song' link
+    $(".js-last").on("click", function(event) {
+        // On click, prevent default and pull in song id from form field
+        event.preventDefault();
+        lastSong();
+    });
 }
 
 function nextSong() {
     let nextId = parseInt($(".js-next").attr("data-id")) + 1;
     //Obtain song data with jQuery call to JSON object, parsing response back into HTML fields
-    $.get("/songs/" + nextId + ".json", function(data) {
-        updateSongData(data, nextId);
+    $.get("/songs/" + nextId + ".json", function(song) {
+        updateSongData(song, nextId, "next");
     });
 }
 
-function updateSongData(song, nextId) {
+function lastSong() {
+    let lastId = parseInt($(".js-last").attr("data-id")) - 1;
+    //Obtain song data with jQuery call to JSON object, parsing response back into HTML fields
+    $.get("/songs/" + lastId + ".json", function(song) {
+        updateSongData(song, lastId, "last");
+    });
+}
+
+function updateSongData(song, newId, type) {
     //Sets html equal to song attributes 
     updateFields(song);
     //Saves url of album cover and converts it to 'icon'
     updateAlbumCover(song);
     //Updates options links with id of new song
     updateOptions(song);
-    //Updates or Removes the 'Next Song' link depending on whether or not another song exists in the collection
-    updateNextSongLink(song, nextId)   
-
+    //Updates or Removes the 'Next Song/Last Song' link depending on whether or not another song exists in the collection
+    if(type === "next") {
+        updateNextSongLink(song, newId)  
+    } else {
+        updateLastSongLink(song, newId)
+    }
 }
 
-function updateNextSongLink(song, nextId) {
+function updateNextSongLink(song, newId) {
     //Checks to see if there is a following song in the database, if so, updates the 'Next Song' link appropriately, if not, removes it. 
-    $.get("/songs/" + (nextId + 1) + ".json", function(song) {
+    $.get("/songs/" + (newId + 1) + ".json", function() {
         $(".js-next").attr("data-id", song["id"]);     
     }).fail(function() {
-        $(".js-next").remove();
+        $(".js-next").hide();
     });
+    //Regardless, update the 'Last Song' link and make sure it is visible
+    $(".js-last").show();
+    $(".js-last").attr("data-id", song["id"]); 
+}
+
+function updateLastSongLink(song, newId) {
+    //Checks to see if there is a preceeding song in the database, if so, updates the 'Last Song' link appropriately, if not, removes it. 
+    $.get("/songs/" + (newId - 1) + ".json", function() {
+        $(".js-last").attr("data-id", song["id"]);     
+    }).fail(function() {
+        $(".js-last").hide();
+    });
+    //Regardless, update the 'Next Song' link and make sure it is visible 
+    $(".js-next").show();
+    $(".js-next").attr("data-id", song["id"]); 
 }
 
 function updateFields(song) {
