2e0954e6e678b02c5e25863274ccc0823881e4ef
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-01-29 13:28:07 -0500

    Added is_last? function to Song, and 'Last Song' link to song#show

--- app/models/song.rb
+++ app/models/song.rb
@@ -40,4 +40,8 @@ class Song < ApplicationRecord
     def self.is_last?(id)
         Song.find_by(id: (id + 1)) ? false : true
     end
+
+    def self.is_first?(id)
+        Song.find_by(id: (id - 1)) ? false : true
+    end
 end
