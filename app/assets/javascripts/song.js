695b68212d68f909a6311906ac32cfed70242906
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-01-29 12:02:01 -0500

    Refactored song.js, split jQuery into distinct function calls

--- app/assets/javascripts/song.js
+++ app/assets/javascripts/song.js
@@ -1,29 +1,57 @@
 $(document).ready(function () {
   $(".js-next").on("click", function(event) {
-    //debugger;
     // On click, prevent default and pull in song id from form field
     event.preventDefault();
+    nextSong();
+  });
+});
+
+function nextSong() {
     let nextId = parseInt($(".js-next").attr("data-id")) + 1;
     //Obtain song data with jQuery call to JSON object, parsing response back into HTML fields
     $.get("/songs/" + nextId + ".json", function(data) {
-        let song = data;
-        $("#songTitle")[0].innerHTML = song["name"];
-        $("#songArtist")[0].innerHTML = song["artist"]["name"];
-        $("#songAlbum")[0].innerHTML = song["album"]["name"];
-        //Saves url of album cover and converts it to 'icon'
-        let image_url =  song["album"]["album_cover"];
-        let icon_url = image_url.replace("original", "icon");
-        $("#songAlbumCover").attr("src", icon_url);
-        $("#songGenre")[0].innerHTML = song["genre"]["name"];
-        //Updates options links with id of new song
-        $("#editSong").attr("href", ("/songs/" + song["id"] + "/edit"));
-        $("#deleteSong").attr("href", ("/songs/" + song["id"]));
-        //Checks to see if there is a following song in the database, if so, updates the 'Next Song' link appropriately, if not, removes it. 
-        $.get("/songs/" + (nextId + 1) + ".json", function(data) {
-            $(".js-next").attr("data-id", song["id"]);     
-        }).fail(function() {
-            $(".js-next").remove();
-        });   
+        updateSongData(data, nextId);
     });
-  });
-});
+}
+
+function updateSongData(song, nextId) {
+    //Sets html equal to song attributes 
+    updateFields(song);
+    //Saves url of album cover and converts it to 'icon'
+    updateAlbumCover(song);
+    //Updates options links with id of new song
+    updateOptions(song);
+    //Updates or Removes the 'Next Song' link depending on whether or not another song exists in the collection
+    updateNextSongLink(song, nextId)   
+
+}
+
+function updateNextSongLink(data, nextId) {
+    //Checks to see if there is a following song in the database, if so, updates the 'Next Song' link appropriately, if not, removes it. 
+    $.get("/songs/" + (nextId + 1) + ".json", function(data) {
+        $(".js-next").attr("data-id", song["id"]);     
+    }).fail(function() {
+        $(".js-next").remove();
+    });
+}
+
+function updateFields(song) {
+    //Sets html equal to song attributes 
+    $("#songTitle")[0].innerHTML = song["name"];
+    $("#songArtist")[0].innerHTML = song["artist"]["name"];
+    $("#songAlbum")[0].innerHTML = song["album"]["name"];
+    $("#songGenre")[0].innerHTML = song["genre"]["name"];
+}
+
+function updateAlbumCover(song) {
+    //Saves url of album cover and converts it to 'icon'
+    let image_url =  song["album"]["album_cover"];
+    let icon_url = image_url.replace("original", "icon");
+    $("#songAlbumCover").attr("src", icon_url);
+}
+
+function updateOptions(song) {
+    //Updates options links with id of new song
+    $("#editSong").attr("href", ("/songs/" + song["id"] + "/edit"));
+    $("#deleteSong").attr("href", ("/songs/" + song["id"]));
+}
