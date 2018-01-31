57a1a8c9e60adc3026766c9c3d7aff36639e1f3f
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-01-31 10:30:35 -0500

    Created Song JS Model Object with prototype method for songList creating

--- app/assets/javascripts/templates/songs.js
+++ app/assets/javascripts/templates/songs.js
@@ -2,16 +2,38 @@ $(document).on("ready turbolinks:load", function() {
 	if($(".albums.show").length !== 0) getSongs();
 });
 
+function Song(id, name, album_id) {
+	this.id = id;
+	this.name = name;
+	this.album_id = album_id;
+}
+
+Song.prototype.getTime = function(length) {
+	//Convert length into minutes and seconds
+    let mins = Math.floor(length / 60);
+    let secs = length % 60;
+    //Return the formatted song time
+    this.time = `${mins}:${secs}`;
+}
+
 function loadSongs(songs) {
-	debugger;
-  var template = Handlebars.compile(document.getElementById("songLi-template").innerHTML);
-  var result = template(songs);
-  $("#songList")[0].innerHTML = result;
+	var template = Handlebars.compile(document.getElementById("songLi-template").innerHTML);
+	var result = template(songs);
+	$("#songList")[0].innerHTML = result;
 }
 
 function getSongs() {
-	let albumId = parseInt($("#songList").attr("album-id"))
-	$.get(albumId + ".json", function(album) {
-		loadSongs(album["songs"]);
+	let album_id = parseInt($("#songList").attr("album-id"))
+	$.get(album_id + ".json", function(album) {
+		loadSongs(createSongs(album["songs"]));
 	});
+}
+
+function createSongs(json_songs) {
+	var songs = json_songs.map(function (s) {
+    	let song = new Song(s.id, s.name, s.album_id);
+    	song.getTime(s.length);
+    	return song;
+  	});
+	return songs;
 }
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :name, :time, :album_id
+  attributes :id, :name, :length, :album_id
   belongs_to :album
   belongs_to :artist
   has_one :genre
