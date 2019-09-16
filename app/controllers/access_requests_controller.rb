9ffa1733b0e640af8cc0725b5ffa0e4943649939
Dembe Koi <stephanos@goldmail.etsu.edu>
2019-09-16 11:24:57 -0400

    Added protections against creating multiple access requests

--- app/controllers/access_requests_controller.rb
+++ app/controllers/access_requests_controller.rb
@@ -2,14 +2,19 @@ class AccessRequestsController < ApplicationController
   protect_from_forgery with: :exception
     
   	def new
-      access_request = AccessRequest.create(requester_id: params[:currentUserId], requested_id: params[:userId], isApproved: false)
-	      if access_request.save
-	        flash[:notice] = 'Request was made successfully.'
-	        redirect_to "/home"
-	      else
-	        flash[:notice] = @access_request.errors
-	        redirect_to "/home"
-	      end
+  		if AccessRequest.where(requester_id: params[:currentUserId], requested_id: params[:userId]).empty? == false
+  			flash[:notice] = 'Already Requested.'
+  			redirect_to "/home"
+  		else
+      		access_request = AccessRequest.create(requester_id: params[:currentUserId], requested_id: params[:userId], isApproved: false)
+	      	if access_request.save
+	        	flash[:notice] = 'Request was made successfully.'
+	        	redirect_to "/home"
+	      	else
+	        	flash[:notice] = @access_request.errors
+	        	redirect_to "/home"
+	      	end
+	    end
   	end
 
   	def approve
