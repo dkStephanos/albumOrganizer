2175db83b9c688d1ed739565281c0d14b3613855
Dembe Koi <stephanos@goldmail.etsu.edu>
2019-09-16 10:41:19 -0400

    Now redirecting to home page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     
     def welcome
-        redirect_to user_path(current_user) if user_signed_in?
+        redirect_to "/home" if user_signed_in?
     end
     
     
